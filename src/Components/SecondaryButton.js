88e67879f0cb40debb51a17bbafbd0220715f732
annbabicheva <164772496+annbabicheva@users.noreply.github.com>
2024-03-24 20:35:39 +0000

    Обновление по чек-листу

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,18 +3,19 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '36px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
+        background: ;
         border: 1px solid #DFDFDF;
+        border-radius: 24px;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
-        color: #152842;
+        color: #FFFFFF;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: #FFFFFF;
         }
         &:active {
-            background: #DFDFDF;
+            background: #FFFFFF;
         }
     `;
     return (
