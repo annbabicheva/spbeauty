006553da90bd6245f60b895fef4abdc6b0aa9137
annbabicheva <164772496+annbabicheva@users.noreply.github.com>
2024-03-30 19:14:16 +0000

    Кнопку спустила вниз

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,13 +3,15 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-1 mt-2 col address-block'>
-            <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-4'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <div className='d-flex flex-column mt-auto p-2 bd-highlight-5'><SecondaryButton>Добавить филиал</SecondaryButton></div>
+        <div className='d-inline-flex flex-column gap-2 mt-4 border rounded-3 p-4 justify-content-between col'>
+            <div>
+                <div className='title'>Адреса заведений</div>
+                <div className='d-inline-flex flex-column gap-2 mt-4'>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                </div>
             </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
     );
 }
